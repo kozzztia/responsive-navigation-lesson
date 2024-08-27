8a096d0636f17ae3574050604e0916566ce9778b
kozzztia <kozzztia@gmail.com>
2024-08-27 15:03:45 +0300

    change function + css user-select

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const btn = document.querySelector('#toggle');
 const aside = document.querySelector('aside');
 const main = document.querySelector('main');
 
-btn.addEventListener('click', toggleStyles)
+// btn.addEventListener('click', toggleStyles);
+btn.onclick=  toggleStyles
 
 function toggleStyles() {
     aside.classList.toggle('close');
